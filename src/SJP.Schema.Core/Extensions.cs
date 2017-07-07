cabb6a3a91cbe7ea9d0adb4fca98d190e051102f
Simon Potter <simon@sjp.co.nz>
2017-07-07 16:42:27 -0400

    Add more read-only collection extension methods.

--- src/SJP.Schema.Core/Extensions.cs
+++ src/SJP.Schema.Core/Extensions.cs
@@ -70,8 +70,80 @@ namespace SJP.Schema.Core
         }
     }
 
-    public static class ReadOnlyExtensions
+    public static class ReadOnlyCollectionExtensions
     {
+        public static IReadOnlyCollection<T> AsReadOnlyCollection<T>(this ICollection<T> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return new ReadOnlyCollection<T>(source.ToList());
+        }
+
+        public static IReadOnlyList<T> AsReadOnlyList<T>(this IList<T> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.ToList().AsReadOnly();
+        }
+
+        public static IReadOnlyList<T> ToReadOnlyList<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.ToList().AsReadOnly();
+        }
+
+        public static IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<TSource, TKey, TValue>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TValue> valueSelector, IEqualityComparer<TKey> comparer)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+            if (valueSelector == null)
+                throw new ArgumentNullException(nameof(valueSelector));
+            if (comparer == null)
+                throw new ArgumentNullException(nameof(comparer));
+
+            var dict = source.ToDictionary(keySelector, valueSelector, comparer);
+            return new ReadOnlyDictionary<TKey, TValue>(dict);
+        }
+
+        public static IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<TSource, TKey, TValue>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TValue> valueSelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+            if (valueSelector == null)
+                throw new ArgumentNullException(nameof(valueSelector));
+
+            var dict = source.ToDictionary(keySelector, valueSelector);
+            return new ReadOnlyDictionary<TKey, TValue>(dict);
+        }
+
+        public static IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source, IEqualityComparer<TKey> comparer)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (comparer == null)
+                throw new ArgumentNullException(nameof(comparer));
+
+            var dict = source.ToDictionary(kv => kv.Key, kv => kv.Value, comparer);
+            return new ReadOnlyDictionary<TKey, TValue>(dict);
+        }
+
+        public static IReadOnlyDictionary<TKey, TValue> ToReadOnlyDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var dict = source.ToDictionary(kv => kv.Key, kv => kv.Value);
+            return new ReadOnlyDictionary<TKey, TValue>(dict);
+        }
+
         public static IReadOnlyDictionary<TKey, TValue> AsReadOnlyDictionary<TKey, TValue>(this IDictionary<TKey, TValue> source)
         {
             if (source == null)
