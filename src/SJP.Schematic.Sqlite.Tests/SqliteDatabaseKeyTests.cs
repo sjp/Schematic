83e1b490f5d0e7a3f977205733b9756873f2a1a5
Simon Potter <simon@sjp.co.nz>
2017-09-28 20:43:17 -0400

    Add tests for sqlite keys.

---
+++ src/SJP.Schematic.Sqlite.Tests/SqliteDatabaseKeyTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using SJP.Schematic.Core;
+using System.Linq;
+
+namespace SJP.Schematic.Sqlite.Tests
+{
+    [TestFixture]
+    internal class SqliteDatabaseKeyTests
+    {
+        [Test]
+        public void Ctor_GivenNullTable_ThrowsArgumentNullException()
+        {
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            Assert.Throws<ArgumentNullException>(() => new SqliteDatabaseKey(null, keyName, keyType, columns));
+        }
+
+        [Test]
+        public void Ctor_GivenNullName_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            Assert.Throws<ArgumentNullException>(() => new SqliteDatabaseKey(table, null, keyType, columns));
+        }
+
+        [Test]
+        public void Ctor_GivenNameMissingLocalIdentifier_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            var keyName = new SchemaIdentifier("test_key");
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            Assert.Throws<ArgumentNullException>(() => new SqliteDatabaseKey(table, keyName, keyType, columns));
+        }
+
+        [Test]
+        public void Ctor_GivenInvalidDatabaseKeyType_ThrowsArgumentException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = (DatabaseKeyType)55;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            Assert.Throws<ArgumentException>(() => new SqliteDatabaseKey(table, keyName, keyType, columns));
+        }
+
+        [Test]
+        public void Ctor_GivenNullColumnSet_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+
+            Assert.Throws<ArgumentNullException>(() => new SqliteDatabaseKey(table, keyName, keyType, null));
+        }
+
+        [Test]
+        public void Ctor_GivenEmptyColumnSet_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var columns = Enumerable.Empty<IDatabaseColumn>();
+
+            Assert.Throws<ArgumentNullException>(() => new SqliteDatabaseKey(table, keyName, keyType, columns));
+        }
+
+        [Test]
+        public void Ctor_GivenColumnSetContainingNullColumn_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var columns = new IDatabaseColumn[] { null };
+
+            Assert.Throws<ArgumentNullException>(() => new SqliteDatabaseKey(table, keyName, keyType, columns));
+        }
+
+        [Test]
+        public void Table_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            var key = new SqliteDatabaseKey(table, keyName, keyType, columns);
+
+            Assert.AreEqual(table, key.Table);
+        }
+
+        [Test]
+        public void Name_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            var key = new SqliteDatabaseKey(table, keyName, keyType, columns);
+
+            Assert.AreEqual(keyName, key.Name);
+        }
+
+        [Test]
+        public void KeyType_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            var key = new SqliteDatabaseKey(table, keyName, keyType, columns);
+
+            Assert.AreEqual(keyType, key.KeyType);
+        }
+
+        [Test]
+        public void Columns_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+
+            var key = new SqliteDatabaseKey(table, keyName, keyType, columns);
+
+            Assert.AreEqual(columns, key.Columns);
+        }
+
+        [Test]
+        public void IsEnabled_PropertyGet_ReturnsTrue()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            var key = new SqliteDatabaseKey(table, keyName, keyType, columns);
+
+            Assert.AreEqual(enabled, key.IsEnabled);
+        }
+    }
+}
