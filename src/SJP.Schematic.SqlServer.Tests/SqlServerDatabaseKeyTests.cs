789d5d32aaf8fbbf8df19560126f501a82ff569a
Simon Potter <simon@sjp.co.nz>
2017-09-28 20:26:18 -0400

    Relational and database key tests.

---
+++ src/SJP.Schematic.SqlServer.Tests/SqlServerDatabaseKeyTests.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using SJP.Schematic.Core;
+using System.Linq;
+
+namespace SJP.Schematic.SqlServer.Tests
+{
+    [TestFixture]
+    internal class SqlServerDatabaseKeyTests
+    {
+        [Test]
+        public void Ctor_GivenNullTable_ThrowsArgumentNullException()
+        {
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerDatabaseKey(null, keyName, keyType, columns, enabled));
+        }
+
+        [Test]
+        public void Ctor_GivenNullName_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerDatabaseKey(table, null, keyType, columns, enabled));
+        }
+
+        [Test]
+        public void Ctor_GivenNameMissingLocalIdentifier_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            var keyName = new SchemaIdentifier("test_key");
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled));
+        }
+
+        [Test]
+        public void Ctor_GivenInvalidDatabaseKeyType_ThrowsArgumentException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = (DatabaseKeyType)55;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentException>(() => new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled));
+        }
+
+        [Test]
+        public void Ctor_GivenNullColumnSet_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerDatabaseKey(table, keyName, keyType, null, enabled));
+        }
+
+        [Test]
+        public void Ctor_GivenEmptyColumnSet_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var columns = Enumerable.Empty<IDatabaseColumn>();
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled));
+        }
+
+        [Test]
+        public void Ctor_GivenColumnSetContainingNullColumn_ThrowsArgumentNullException()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var columns = new IDatabaseColumn[] { null };
+            const bool enabled = true;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled));
+        }
+
+        [Test]
+        public void Table_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            var key = new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled);
+
+            Assert.AreEqual(table, key.Table);
+        }
+
+        [Test]
+        public void Name_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Primary;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            var key = new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled);
+
+            Assert.AreEqual(keyName, key.Name);
+        }
+
+        [Test]
+        public void KeyType_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            var key = new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled);
+
+            Assert.AreEqual(keyType, key.KeyType);
+        }
+
+        [Test]
+        public void Columns_PropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            var key = new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled);
+
+            Assert.AreEqual(columns, key.Columns);
+        }
+
+        [Test]
+        public void IsEnabled_GivenTrueCtorArgAndPropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = true;
+
+            var key = new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled);
+
+            Assert.AreEqual(enabled, key.IsEnabled);
+        }
+
+        [Test]
+        public void IsEnabled_GivenFalseCtorArgAndPropertyGet_EqualsCtorArg()
+        {
+            var table = Mock.Of<IRelationalDatabaseTable>();
+            Identifier keyName = "test_key";
+            const DatabaseKeyType keyType = DatabaseKeyType.Foreign;
+            var column = Mock.Of<IDatabaseColumn>();
+            var columns = new[] { column };
+            const bool enabled = false;
+
+            var key = new SqlServerDatabaseKey(table, keyName, keyType, columns, enabled);
+
+            Assert.AreEqual(enabled, key.IsEnabled);
+        }
+    }
+}
---
+++ src/SJP.Schematic.SqlServer.Tests/SqlServerRelationalKeyTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using SJP.Schematic.Core;
+
+namespace SJP.Schematic.SqlServer.Tests
+{
+    [TestFixture]
+    internal class SqlServerRelationalKeyTests
+    {
+        [Test]
+        public void Ctor_GivenNullChildKey_ThrowsArgumentNullException()
+        {
+            var parentKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.NoAction;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.NoAction;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerRelationalKey(null, parentKey, deleteAction, updateAction));
+        }
+
+        [Test]
+        public void Ctor_GivenNullParentKey_ThrowsArgumentNullException()
+        {
+            var childKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.NoAction;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.NoAction;
+
+            Assert.Throws<ArgumentNullException>(() => new SqlServerRelationalKey(childKey, null, deleteAction, updateAction));
+        }
+
+        [Test]
+        public void Ctor_GivenInvalidDeleteAction_ThrowsArgumentException()
+        {
+            var childKey = Mock.Of<IDatabaseKey>();
+            var parentKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = (RelationalKeyUpdateAction)55;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.NoAction;
+
+            Assert.Throws<ArgumentException>(() => new SqlServerRelationalKey(childKey, parentKey, deleteAction, updateAction));
+        }
+
+        [Test]
+        public void Ctor_GivenInvalidUpdateAction_ThrowsArgumentException()
+        {
+            var childKey = Mock.Of<IDatabaseKey>();
+            var parentKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.NoAction;
+            const RelationalKeyUpdateAction updateAction = (RelationalKeyUpdateAction)55;
+
+            Assert.Throws<ArgumentException>(() => new SqlServerRelationalKey(childKey, parentKey, deleteAction, updateAction));
+        }
+
+        [Test]
+        public void ChildKey_PropertyGet_EqualsCtorArg()
+        {
+            var parentKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.Cascade;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.SetDefault;
+
+            Identifier keyName = "test_child_key";
+            var childKey = new Mock<IDatabaseKey>();
+            childKey.Setup(t => t.Name).Returns(keyName);
+            var childKeyArg = childKey.Object;
+
+            var relationalKey = new SqlServerRelationalKey(childKeyArg, parentKey, deleteAction, updateAction);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(keyName, relationalKey.ChildKey.Name);
+                Assert.AreSame(childKeyArg, relationalKey.ChildKey);
+            });
+        }
+
+        [Test]
+        public void ParentKey_PropertyGet_EqualsCtorArg()
+        {
+            var childKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.Cascade;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.SetDefault;
+
+            Identifier keyName = "test_parent_key";
+            var parentKey = new Mock<IDatabaseKey>();
+            parentKey.Setup(t => t.Name).Returns(keyName);
+            var parentKeyArg = parentKey.Object;
+
+            var relationalKey = new SqlServerRelationalKey(childKey, parentKeyArg, deleteAction, updateAction);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(keyName, relationalKey.ParentKey.Name);
+                Assert.AreSame(parentKeyArg, relationalKey.ParentKey);
+            });
+        }
+
+        [Test]
+        public void DeleteAction_PropertyGet_EqualsCtorArg()
+        {
+            var childKey = Mock.Of<IDatabaseKey>();
+            var parentKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.Cascade;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.SetDefault;
+
+            var relationalKey = new SqlServerRelationalKey(childKey, parentKey, deleteAction, updateAction);
+
+            Assert.AreEqual(deleteAction, relationalKey.DeleteAction);
+        }
+
+        [Test]
+        public void UpdateAction_PropertyGet_EqualsCtorArg()
+        {
+            var childKey = Mock.Of<IDatabaseKey>();
+            var parentKey = Mock.Of<IDatabaseKey>();
+            const RelationalKeyUpdateAction deleteAction = RelationalKeyUpdateAction.Cascade;
+            const RelationalKeyUpdateAction updateAction = RelationalKeyUpdateAction.SetDefault;
+
+            var relationalKey = new SqlServerRelationalKey(childKey, parentKey, deleteAction, updateAction);
+
+            Assert.AreEqual(updateAction, relationalKey.UpdateAction);
+        }
+    }
+}
