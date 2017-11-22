761ee0740ff0c81145143d673b358158019df036
Simon Potter <simon@sjp.co.nz>
2017-11-21 20:52:10 -0500

    Add type param for type providers instead of providing raw type objects.

---
+++ src/SJP.Schematic.Modelled.Reflection/ReflectionTableTypeProviderT.cs
@@ -0,0 +1,14 @@
+﻿using SJP.Schematic.Core;
+
+namespace SJP.Schematic.Modelled.Reflection
+{
+    public class ReflectionTableTypeProvider<T> : ReflectionTableTypeProvider where T : new()
+    {
+        public ReflectionTableTypeProvider(IDatabaseDialect dialect)
+            : base(dialect, typeof(T))
+        {
+        }
+
+        public new T TableInstance => (T)base.TableInstance;
+    }
+}
