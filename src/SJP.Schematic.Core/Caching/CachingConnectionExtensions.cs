d2ac0b22a96bb0365f0d4ffa471029fa9053b2f2
Simon Potter <simon@sjp.co.nz>
2017-11-27 21:13:43 -0500

    Document caching extension methods.

--- src/SJP.Schematic.Core/Caching/CachingConnectionExtensions.cs
+++ src/SJP.Schematic.Core/Caching/CachingConnectionExtensions.cs
@@ -4,8 +4,17 @@ using System.Data.Common;
 
 namespace SJP.Schematic.Core.Caching
 {
+    /// <summary>
+    /// Extension methods to make creation of caching objects easier.
+    /// </summary>
     public static class CachingExtensions
     {
+        /// <summary>
+        /// Creates a caching connection whose results will be stored in an in-memory cache.
+        /// </summary>
+        /// <param name="connection">A connection whose results will be cached.</param>
+        /// <returns>An <see cref="IDbConnection"/> instance that will cache query results.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="connection"/> is <c>null</c>.</exception>
         public static IDbConnection AsCachedConnection(this IDbConnection connection)
         {
             if (connection == null)
@@ -15,6 +24,13 @@ namespace SJP.Schematic.Core.Caching
             return new CachingConnection(dbConnection);
         }
 
+        /// <summary>
+        /// Creates a caching connection whose results will be stored in a provided caching store.
+        /// </summary>
+        /// <param name="connection">A connection whose results will be cached.</param>
+        /// <param name="cacheStore">A caching store, where query results will be stored.</param>
+        /// <returns>An <see cref="IDbConnection"/> instance that will cache query results.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="connection"/> or <paramref name="cacheStore"/> is <c>null</c>.</exception>
         public static IDbConnection AsCachedConnection(this IDbConnection connection, ICacheStore<int, DataTable> cacheStore)
         {
             if (connection == null)
