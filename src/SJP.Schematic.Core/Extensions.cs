72648435135f19bd6c27dd605703739a6eabf74f
Simon Potter <simon@sjp.co.nz>
2018-02-26 21:30:27 -0500

    Add new Empty<T> method to accept a predicate.

--- src/SJP.Schematic.Core/Extensions.cs
+++ src/SJP.Schematic.Core/Extensions.cs
@@ -41,6 +41,8 @@ namespace SJP.Schematic.Core
     {
         public static bool Empty<T>(this IEnumerable<T> source) => !source.Any();
 
+        public static bool Empty<T>(this IEnumerable<T> source, Func<T, bool> predicate) => !source.Any(predicate);
+
         public static bool AnyNull<T>(this IEnumerable<T> source) where T : class => source.Any(x => x == null);
 
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) => source.DistinctBy(keySelector, null);
@@ -48,8 +50,10 @@ namespace SJP.Schematic.Core
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
            Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
 
             return _(); IEnumerable<TSource> _()
             {
