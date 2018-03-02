ba4020c1bfe6c167cbe9b0c333240c57803ccdd0
Simon Potter <simon@sjp.co.nz>
2018-03-01 21:48:11 -0500

    Add a lint rule that checks for correctness of view definitions.

---
+++ src/SJP.Schematic.Lint/Rules/InvalidViewDefinitionRule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using SJP.Schematic.Core;
+
+namespace SJP.Schematic.Lint.Rules
+{
+    public class InvalidViewDefinitionRule : Rule
+    {
+        public InvalidViewDefinitionRule(IDbConnection connection, RuleLevel level)
+            : base(RuleTitle, level)
+        {
+            Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        protected IDbConnection Connection { get; }
+
+        public override IEnumerable<IRuleMessage> AnalyseDatabase(IRelationalDatabase database)
+        {
+            if (database == null)
+                throw new ArgumentNullException(nameof(database));
+
+            var dialect = database.Dialect;
+            if (dialect == null)
+                throw new ArgumentException("The dialect on the given database is null.", nameof(database));
+
+            return database.Views.SelectMany(v => AnalyseView(dialect, v)).ToList();
+        }
+
+        protected IEnumerable<IRuleMessage> AnalyseView(IDatabaseDialect dialect, IRelationalDatabaseView view)
+        {
+            if (dialect == null)
+                throw new ArgumentNullException(nameof(dialect));
+            if (view == null)
+                throw new ArgumentNullException(nameof(view));
+
+            try
+            {
+                var quotedViewName = dialect.QuoteName(view.Name);
+                var query = "select 1 as tmp from " + quotedViewName;
+                var result = Connection.ExecuteScalar(query);
+
+                return Enumerable.Empty<IRuleMessage>();
+            }
+            catch
+            {
+                var messageText = $"The view { view.Name } was unable to be queried. This may indicate an incorrect view definition.";
+                var ruleMessage = new RuleMessage(RuleTitle, Level, messageText);
+                return new[] { ruleMessage };
+            }
+        }
+
+        private const string RuleTitle = "Invalid view definition.";
+    }
+}
