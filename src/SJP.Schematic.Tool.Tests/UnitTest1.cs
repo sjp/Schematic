0c16bb8b2f7728f790196189a7de90396219c00a
Simon Potter <simon@sjp.co.nz>
2018-06-05 19:42:21 -0400

    Initial commit of command line tool.
    Not ready for use yet.

---
+++ src/SJP.Schematic.Tool.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace SJP.Schematic.Tool.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
