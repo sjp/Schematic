12ae9720e12e21c414221fa04a4f8e3216326cf5
Simon Potter <simon@sjp.co.nz>
2018-09-25 20:47:38 -0400

    Add basic identity resolution strategy.

---
+++ src/SJP.Schematic.Core.Tests/VerbatimIdentifierResolutionStrategyTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SJP.Schematic.Core.Tests
+{
+    [TestFixture]
+    internal static class VerbatimIdentifierResolutionStrategyTests
+    {
+        [Test]
+        public static void GetResolutionOrder_GivenNullIdentifier_ThrowsArgumentNullException()
+        {
+            var resolver = new VerbatimIdentifierResolutionStrategy();
+            Assert.Throws<ArgumentNullException>(() => resolver.GetResolutionOrder(null));
+        }
+
+        [Test]
+        public static void GetResolutionOrder_GivenIdentifier_ReturnsIdenticalIdentifier()
+        {
+            var resolver = new VerbatimIdentifierResolutionStrategy();
+            var input = new Identifier("A", "B", "C", "D");
+
+            var results = resolver.GetResolutionOrder(input).ToList();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(1, results.Count);
+                Assert.AreEqual(input, results[0]);
+            });
+        }
+    }
+}
---
+++ src/SJP.Schematic.Core/VerbatimIdentifierResolutionStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SJP.Schematic.Core
+{
+    public class VerbatimIdentifierResolutionStrategy : IIdentifierResolutionStrategy
+    {
+        public IEnumerable<Identifier> GetResolutionOrder(Identifier identifier)
+        {
+            if (identifier == null)
+                throw new ArgumentNullException(nameof(identifier));
+
+            return new[] { identifier };
+        }
+    }
+}
