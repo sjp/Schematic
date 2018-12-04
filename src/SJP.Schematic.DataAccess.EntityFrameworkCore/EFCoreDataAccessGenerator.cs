7d59f8a035a8a73daee5d70ecf13e2f1372bf213
Simon Potter <simon@sjp.co.nz>
2018-12-04 14:31:26 +1300

    Bump dependencies.

--- src/SJP.Schematic.DataAccess.EntityFrameworkCore/EFCoreDataAccessGenerator.cs
+++ src/SJP.Schematic.DataAccess.EntityFrameworkCore/EFCoreDataAccessGenerator.cs
@@ -68,7 +68,7 @@ namespace SJP.Schematic.DataAccess.EntityFrameworkCore
     </PropertyGroup>
 
     <ItemGroup>
-        <PackageReference Include=""Microsoft.EntityFrameworkCore.Relational"" Version=""2.1.3"" />
+        <PackageReference Include=""Microsoft.EntityFrameworkCore.Relational"" Version=""2.2.0"" />
     </ItemGroup>
 </Project>";
     }
