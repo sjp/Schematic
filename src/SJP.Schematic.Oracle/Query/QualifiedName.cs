e6c6a422059093892188b0041cc87834137b8e9e
Simon Potter <simon@sjp.co.nz>
2018-12-05 16:34:47 +1300

    Fix bug in primary key change to option. Remove unused query prop.

--- src/SJP.Schematic.Oracle/Query/QualifiedName.cs
+++ src/SJP.Schematic.Oracle/Query/QualifiedName.cs
@@ -2,8 +2,6 @@
 {
     public class QualifiedName
     {
-        public string DatabaseName { get; set; }
-
         public string SchemaName { get; set; }
 
         public string ObjectName { get; set; }
