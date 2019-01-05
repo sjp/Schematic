a1d98b1a29dd4b919321564336a33f4a3e76f2bb
Simon Potter <simon@sjp.co.nz>
2019-01-05 16:58:08 +1300

    Add a version resolving dictionary.
    This is in preparation for creating versioned providers of database objects. These providers enable multiple versions of a given vendor to be supported.

---
+++ src/SJP.Schematic.Core.Tests/Utilities/VersionResolvingDictionaryTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using SJP.Schematic.Core.Utilities;
+
+namespace SJP.Schematic.Core.Tests.Utilities
+{
+    [TestFixture]
+    internal static class VersionResolvingDictionaryTests
+    {
+        [Test]
+        public static void Ctor_GivenNullDictionary_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new VersionResolvingDictionary<int>(null));
+        }
+
+        [Test]
+        public static void Ctor_GivenEmptyDictionary_ThrowsArgumentException()
+        {
+            var dictionary = new Dictionary<Version, int>();
+            Assert.Throws<ArgumentException>(() => new VersionResolvingDictionary<int>(dictionary));
+        }
+
+        [Test]
+        public static void GetValue_GivenNullVersion_ThrowsArgmentNullException()
+        {
+            var dictionary = new Dictionary<Version, int>
+            {
+                [new Version(1, 0)] = 1,
+                [new Version(1, 1)] = 2
+            };
+            var versionDictionary = new VersionResolvingDictionary<int>(dictionary);
+
+            Assert.Throws<ArgumentNullException>(() => versionDictionary.GetValue(null));
+        }
+
+        [Test]
+        public static void GetValue_GivenVersionLessThanLowestInCtor_ReturnsLowestInCtor()
+        {
+            var dictionary = new Dictionary<Version, int>
+            {
+                [new Version(1, 0)] = 1,
+                [new Version(1, 1)] = 2
+            };
+            var versionDictionary = new VersionResolvingDictionary<int>(dictionary);
+
+            var value = versionDictionary.GetValue(new Version(0, 1));
+
+            Assert.AreEqual(1, value);
+        }
+
+        [Test]
+        public static void GetValue_GivenVersionMoreThanHighestInCtor_ReturnsHighestInCtor()
+        {
+            var dictionary = new Dictionary<Version, int>
+            {
+                [new Version(1, 0)] = 1,
+                [new Version(1, 1)] = 2
+            };
+            var versionDictionary = new VersionResolvingDictionary<int>(dictionary);
+
+            var value = versionDictionary.GetValue(new Version(2, 0));
+
+            Assert.AreEqual(2, value);
+        }
+
+        [Test]
+        public static void GetValue_GivenVersionEqualToMiddle_ReturnsValueFromMiddleVersion()
+        {
+            var dictionary = new Dictionary<Version, int>
+            {
+                [new Version(1, 0)] = 1,
+                [new Version(1, 1)] = 2,
+                [new Version(2, 0)] = 3
+            };
+            var versionDictionary = new VersionResolvingDictionary<int>(dictionary);
+
+            var value = versionDictionary.GetValue(new Version(1, 1));
+
+            Assert.AreEqual(2, value);
+        }
+
+        [Test]
+        public static void GetValue_GivenVersionAboveMiddle_ReturnsValueFromMiddleVersion()
+        {
+            var dictionary = new Dictionary<Version, int>
+            {
+                [new Version(1, 0)] = 1,
+                [new Version(1, 1)] = 2,
+                [new Version(2, 0)] = 3
+            };
+            var versionDictionary = new VersionResolvingDictionary<int>(dictionary);
+
+            var value = versionDictionary.GetValue(new Version(1, 5));
+
+            Assert.AreEqual(2, value);
+        }
+    }
+}
---
+++ src/SJP.Schematic.Core/Utilities/VersionResolvingDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SJP.Schematic.Core.Extensions;
+
+namespace SJP.Schematic.Core.Utilities
+{
+    public class VersionResolvingDictionary<T>
+    {
+        public VersionResolvingDictionary(IReadOnlyDictionary<Version, T> lookup)
+        {
+            if (lookup == null)
+                throw new ArgumentNullException(nameof(lookup));
+            if (lookup.Empty())
+                throw new ArgumentException("At least one value must be present in the given lookup.", nameof(lookup));
+
+            _lookup = lookup;
+        }
+
+        public T GetValue(Version version)
+        {
+            if (version == null)
+                throw new ArgumentNullException(nameof(version));
+
+            var versionKeys = _lookup.Keys.OrderBy(x => x).ToList();
+            var firstVersion = versionKeys[0];
+            if (version <= firstVersion)
+                return _lookup[firstVersion];
+
+            // we want to find the version that's *at least* the version
+            // but we want to use the highest version possible
+            versionKeys.Reverse();
+
+            var matchingVersion = versionKeys.Find(v => version >= v);
+            return _lookup[matchingVersion];
+        }
+
+        private readonly IReadOnlyDictionary<Version, T> _lookup;
+    }
+}
