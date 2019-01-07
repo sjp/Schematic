7b7ea49fb115726659763f74fb437bd6079584a9
Simon Potter <simon@sjp.co.nz>
2019-01-07 15:59:18 +1300

    Enable version-specific testing for SQL Server.
    Testing for each version will be added later (mostly for CI).

---
+++ src/SJP.Schematic.SqlServer.Tests/Integration/Versions/V2008R2/SqlServer2008R2Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using NUnit.Framework;
+using SJP.Schematic.Core;
+using Microsoft.Extensions.Configuration;
+
+namespace SJP.Schematic.SqlServer.Tests.Integration.Versions.V2008R2
+{
+    internal static class Config2008R2
+    {
+        public static IDbConnection Connection { get; } = SqlServerDialect.CreateConnectionAsync(ConnectionString).GetAwaiter().GetResult();
+
+        private static string ConnectionString => Configuration.GetConnectionString("TestDb");
+
+        private static IConfigurationRoot Configuration => new ConfigurationBuilder()
+            .AddJsonFile("sqlserver-test-2008R2.json.config")
+            .AddJsonFile("sqlserver-test-2008R2.json.config.local", optional: true)
+            .Build();
+    }
+
+    [Category("SqlServerDatabase")]
+    [Category("SkipWhenLiveUnitTesting")]
+    [TestFixture]
+    internal abstract class SqlServer2008R2Test
+    {
+        protected IDbConnection Connection { get; } = Config2008R2.Connection;
+
+        protected IDatabaseDialect Dialect { get; } = new SqlServerDialect(Config2008R2.Connection);
+
+        protected IIdentifierDefaults IdentifierDefaults { get; } = new SqlServerDialect(Config2008R2.Connection).GetIdentifierDefaultsAsync().GetAwaiter().GetResult();
+    }
+}
---
+++ src/SJP.Schematic.SqlServer.Tests/Integration/Versions/V2016/SqlServer2016Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using NUnit.Framework;
+using SJP.Schematic.Core;
+using Microsoft.Extensions.Configuration;
+
+namespace SJP.Schematic.SqlServer.Tests.Integration.Versions.V2016
+{
+    internal static class Config2016
+    {
+        public static IDbConnection Connection { get; } = SqlServerDialect.CreateConnectionAsync(ConnectionString).GetAwaiter().GetResult();
+
+        private static string ConnectionString => Configuration.GetConnectionString("TestDb");
+
+        private static IConfigurationRoot Configuration => new ConfigurationBuilder()
+            .AddJsonFile("sqlserver-test-2016.json.config")
+            .AddJsonFile("sqlserver-test-2016.json.config.local", optional: true)
+            .Build();
+    }
+
+    [Category("SqlServerDatabase")]
+    [Category("SkipWhenLiveUnitTesting")]
+    [TestFixture]
+    internal abstract class SqlServer2016Test
+    {
+        protected IDbConnection Connection { get; } = Config2016.Connection;
+
+        protected IDatabaseDialect Dialect { get; } = new SqlServerDialect(Config2016.Connection);
+
+        protected IIdentifierDefaults IdentifierDefaults { get; } = new SqlServerDialect(Config2016.Connection).GetIdentifierDefaultsAsync().GetAwaiter().GetResult();
+    }
+}
