0dd93139a1b31303b968c0df95d762c33cf053d7
Simon Potter <simon@sjp.co.nz>
2019-01-17 17:50:58 +1300

    Add tests for DistinctBy.

--- src/SJP.Schematic.Core.Tests/Extensions/EnumerableExtensionsTests.cs
+++ src/SJP.Schematic.Core.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using SJP.Schematic.Core.Extensions;
 
@@ -85,6 +86,73 @@ namespace SJP.Schematic.Core.Tests.Extensions
         {
             IEnumerable<string> input = new[] { "A", null, "C" };
             Assert.IsTrue(input.AnyNull());
+        }
+
+        [Test]
+        public static void DistinctBy_GivenNullCollection_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => EnumerableExtensions.DistinctBy<string, string>(null, x => x));
+        }
+
+        [Test]
+        public static void DistinctBy_GivenNullSelector_ThrowsArgumentNullException()
+        {
+            var source = new[] { "first", "second", "third", "fourth", "fifth" };
+
+            Assert.Throws<ArgumentNullException>(() => source.DistinctBy<string, string>(null));
+        }
+
+        [Test]
+        public static void DistinctBy_ForComparerOverloadGivenNullCollection_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => EnumerableExtensions.DistinctBy<string, string>(null, x => x, EqualityComparer<string>.Default));
+        }
+
+        [Test]
+        public static void DistinctBy_ForComparerOverloadGivenNullSelector_ThrowsArgumentNullException()
+        {
+            var source = new[] { "first", "second", "third", "fourth", "fifth" };
+
+            Assert.Throws<ArgumentNullException>(() => source.DistinctBy(null, EqualityComparer<string>.Default));
+        }
+
+        [Test]
+        public static void DistinctBy_GivenValidSelector_ReturnsExpectedResult()
+        {
+            var source = new[] { "first", "second", "third", "fourth", "fifth" };
+            var expected = new[] { "first", "second" };
+
+            var distinct = source.DistinctBy(word => word.Length);
+
+            var areEqual = expected.SequenceEqual(distinct);
+
+            Assert.IsTrue(areEqual);
+        }
+
+        [Test]
+        public static void DistinctBy_GivenCustomComparer_ReturnsExpectedResult()
+        {
+            var source = new[] { "first", "FIRST", "second", "second", "third" };
+            var expected = new[] { "first", "second", "third" };
+
+            var distinct = source.DistinctBy(word => word, StringComparer.OrdinalIgnoreCase);
+
+            var areEqual = expected.SequenceEqual(distinct);
+
+            Assert.IsTrue(areEqual);
+        }
+
+        [Test]
+        public static void DistinctBy_GivenNullComparer_ReturnsSameResultsAsNoParam()
+        {
+            var source = new[] { "first", "second", "third", "fourth", "fifth" };
+            var expected = new[] { "first", "second" };
+
+            var distinct = source.DistinctBy(word => word.Length, null);
+
+            var areEqual = expected.SequenceEqual(distinct);
+
+            Assert.IsTrue(areEqual);
         }
     }
 }
