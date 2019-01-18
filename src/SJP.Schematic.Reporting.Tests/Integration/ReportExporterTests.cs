663fb46df7b2e3c6cf56daa70031bcdab19ecbd5
Simon Potter <simon@sjp.co.nz>
2019-01-18 13:21:09 +1300

    Begin generating reports based on a sqlite sakila database during test runs.

---
+++ src/SJP.Schematic.Reporting.Tests/Integration/ReportExporterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SJP.Schematic.Reporting.Tests.Integration
+{
+    internal sealed class ReportExporterTests : SakilaTest
+    {
+        [Test]
+        public async Task ExportAsync_GivenValidSqliteDatabase_GeneratesReportsWithoutError()
+        {
+            Assert.IsTrue(File.Exists(Config.SakilaDbPath), "Expected to find a database at: " + Config.SakilaDbPath);
+
+            var database = GetDatabase();
+
+            var outDir = string.Empty;
+            try
+            {
+                outDir = Path.Combine(Environment.CurrentDirectory, "SakilaExport");
+                if (Directory.Exists(outDir))
+                    Directory.Delete(outDir, true);
+
+                var exporter = new Html.ReportExporter(Connection, database, outDir);
+                await exporter.ExportAsync().ConfigureAwait(false);
+            }
+            finally
+            {
+                if (Directory.Exists(outDir))
+                    Directory.Delete(outDir, true);
+            }
+        }
+    }
+}
---
+++ src/SJP.Schematic.Reporting.Tests/Integration/SakilaTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using SJP.Schematic.Core;
+using SJP.Schematic.Sqlite;
+using SJP.Schematic.Sqlite.Pragma;
+
+namespace SJP.Schematic.Reporting.Tests.Integration
+{
+    internal static class Config
+    {
+        public static IDbConnection Connection
+        {
+            get
+            {
+                EnsureUnzipped();
+                return SqliteDialect.CreateConnectionAsync(ConnectionString).GetAwaiter().GetResult();
+            }
+        }
+
+        private static string ConnectionString => "Data Source=" + SakilaDbPath;
+
+        private static void EnsureUnzipped()
+        {
+            if (File.Exists(SakilaDbPath))
+                return;
+
+            using (var zipFile = File.OpenRead(SakilaZipPath))
+            using (var archive = new ZipArchive(zipFile))
+            {
+                var dbEntry = archive.Entries.Single();
+                dbEntry.ExtractToFile(SakilaDbPath);
+            }
+        }
+
+        public static string SakilaDbPath => Path.Combine(CurrentDirectory, "sakila.sqlite");
+
+        public static string SakilaZipPath => Path.Combine(CurrentDirectory, "sakila.sqlite.zip");
+
+        private static string CurrentDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    }
+
+    [Category("SkipWhenLiveUnitTesting")]
+    [TestFixture]
+    internal abstract class SakilaTest
+    {
+        protected IDbConnection Connection { get; } = Config.Connection;
+
+        protected IDatabaseDialect Dialect { get; } = new SqliteDialect(Config.Connection);
+
+        protected ISqliteConnectionPragma Pragma { get; } = new ConnectionPragma(new SqliteDialect(Config.Connection), Config.Connection);
+
+        protected IIdentifierDefaults IdentifierDefaults { get; } = new SqliteDialect(Config.Connection).GetIdentifierDefaultsAsync().GetAwaiter().GetResult();
+
+        protected IRelationalDatabase GetDatabase() => new SqliteRelationalDatabase(Dialect, Connection, IdentifierDefaults);
+    }
+}
