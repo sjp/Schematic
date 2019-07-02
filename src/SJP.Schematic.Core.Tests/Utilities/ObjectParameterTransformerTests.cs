7b42599d562543ca3bc359d650c717a11f52b6ae
Simon Potter <simon@sjp.co.nz>
2019-07-02 19:21:00 +1200

    Create utility function for creating a parameter lookup from any object.

---
+++ src/SJP.Schematic.Core.Tests/Utilities/ObjectParameterTransformerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using SJP.Schematic.Core.Utilities;
+
+namespace SJP.Schematic.Core.Tests.Utilities
+{
+    [TestFixture]
+    internal static class ObjectParameterTransformerTests
+    {
+        [Test]
+        public static void ToDictionary_GivenNullObject_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ObjectParameterTransformer.ToDictionary(null));
+        }
+
+        [Test]
+        public static void ToDictionary_GivenEmptyObject_ReturnsEmptyDictionary()
+        {
+            var lookup = ObjectParameterTransformer.ToDictionary(new { });
+
+            Assert.Zero(lookup.Count);
+        }
+
+        [Test]
+        public static void ToDictionary_GivenNonEmptyObject_ReturnsDictionaryWithCorrectKeysAndValues()
+        {
+            const int first = 1;
+            const string second = "second";
+            var lookup = ObjectParameterTransformer.ToDictionary(new { FirstKey = first, SecondKey = second });
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(2, lookup.Count);
+                Assert.AreEqual(first, lookup["FirstKey"]);
+                Assert.AreEqual(second, lookup["SecondKey"]);
+            });
+        }
+    }
+}
---
+++ src/SJP.Schematic.Core/Utilities/ObjectParameterTransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SJP.Schematic.Core.Utilities
+{
+    public static class ObjectParameterTransformer
+    {
+        public static IReadOnlyDictionary<string, object> ToDictionary(object param)
+        {
+            if (param == null)
+                throw new ArgumentNullException(nameof(param));
+
+            var paramType = param.GetType();
+            var paramProps = paramType.GetProperties(PropFlags);
+
+            var result = new Dictionary<string, object>(paramProps.Length);
+
+            foreach (var prop in paramProps)
+                result[prop.Name] = prop.GetValue(param);
+
+            return result;
+        }
+
+        private const BindingFlags PropFlags = BindingFlags.FlattenHierarchy | System.Reflection.BindingFlags.GetProperty | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public;
+    }
+}
