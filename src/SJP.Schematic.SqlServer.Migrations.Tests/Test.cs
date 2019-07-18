85b3433be69eacc20e2fc33c0a1e3ec81ec005be
Simon Potter <simon@sjp.co.nz>
2019-07-18 22:16:18 +1200

    Add dummy tests for build.

---
+++ src/SJP.Schematic.SqlServer.Migrations.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SJP.Schematic.SqlServer.Migrations.Tests
+{
+    [TestFixture]
+    internal static class Test
+    {
+        public static void Pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
