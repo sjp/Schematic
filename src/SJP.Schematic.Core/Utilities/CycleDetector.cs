b45aaff2d9dfc1ac91b4ea0106348409f875acb6
Simon Potter <simon@sjp.co.nz>
2019-07-18 21:15:26 +1200

    Improve cycle detection reporting.
    Additionally, easily determine table insertion/deletion order for data population (assuming no cycles).

---
+++ src/SJP.Schematic.Core/Utilities/CycleDetector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuickGraph;
+using QuickGraph.Algorithms.Search;
+
+namespace SJP.Schematic.Core.Utilities
+{
+    /// <summary>
+    /// Discovers cycles within a database
+    /// </summary>
+    public class CycleDetector
+    {
+        private IList<IEdge<Identifier>> _examinedEdges;
+        private List<IReadOnlyCollection<Identifier>> _cycles;
+
+        public IReadOnlyCollection<IReadOnlyCollection<Identifier>> GetCyclePaths(IReadOnlyCollection<IRelationalDatabaseTable> tables)
+        {
+            if (tables == null)
+                throw new ArgumentNullException(nameof(tables));
+            if (tables.Count == 0)
+                return Array.Empty<IReadOnlyCollection<Identifier>>();
+
+            var graph = new AdjacencyGraph<Identifier, SEquatableEdge<Identifier>>();
+            var tableNames = tables.Select(t => t.Name).Distinct().ToList();
+            graph.AddVertexRange(tableNames);
+
+            var foreignKeys = tables
+                .SelectMany(t => t.ParentKeys)
+                .Where(fk => fk.ChildTable != fk.ParentTable)
+                .ToList();
+            foreach (var foreignKey in foreignKeys)
+                graph.AddEdge(new SEquatableEdge<Identifier>(foreignKey.ChildTable, foreignKey.ParentTable));
+
+            return GetCyclePaths(graph);
+        }
+
+        private IReadOnlyCollection<IReadOnlyCollection<Identifier>> GetCyclePaths(IVertexListGraph<Identifier, SEquatableEdge<Identifier>> graph)
+        {
+            if (graph == null)
+                throw new ArgumentNullException(nameof(graph));
+
+            _examinedEdges = new List<IEdge<Identifier>>();
+            _cycles = new List<IReadOnlyCollection<Identifier>>();
+            var dfs = new DepthFirstSearchAlgorithm<Identifier, SEquatableEdge<Identifier>>(graph);
+
+            try
+            {
+                dfs.ExamineEdge += OnExamineEdge;
+                dfs.BackEdge += OnCyclingEdgeFound;
+                dfs.Compute();
+                return _cycles;
+            }
+            finally
+            {
+                dfs.ExamineEdge -= OnExamineEdge;
+                dfs.BackEdge -= OnCyclingEdgeFound;
+            }
+        }
+
+        private void OnExamineEdge(SEquatableEdge<Identifier> e) => _examinedEdges.Add(e);
+
+        private void OnCyclingEdgeFound(SEquatableEdge<Identifier> e)
+        {
+            var startingNode = e.Target;
+            var nextNode = e.Source;
+
+            var knownNodes = new List<Identifier> { startingNode, nextNode };
+
+            var edges = _examinedEdges.Reverse().Skip(1); // skipping first edge because that's the back edge
+            foreach (var edge in edges)
+            {
+                if (edge.Target != nextNode)
+                    continue;
+
+                if (!knownNodes.Contains(edge.Source))
+                {
+                    knownNodes.Add(edge.Source);
+                    nextNode = edge.Source;
+                }
+                else
+                {
+                    knownNodes.Reverse();
+                    _cycles.Add(knownNodes);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ src/SJP.Schematic.Core/Utilities/TableRelationshipOrderer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuickGraph;
+using QuickGraph.Algorithms;
+using QuickGraph.Algorithms.Search;
+
+namespace SJP.Schematic.Core.Utilities
+{
+    public class TableRelationshipOrderer
+    {
+        public TableRelationshipOrderer(IReadOnlyCollection<IRelationalDatabaseTable> tables)
+        {
+            if (tables == null)
+                throw new ArgumentNullException(nameof(tables));
+
+            var graph = new AdjacencyGraph<Identifier, SEquatableEdge<Identifier>>();
+            var tableNames = tables.Select(t => t.Name).Distinct().ToList();
+            graph.AddVertexRange(tableNames);
+
+            var foreignKeys = tables
+                .SelectMany(t => t.ParentKeys)
+                .Where(fk => fk.ChildTable != fk.ParentTable)
+                .ToList();
+            foreach (var foreignKey in foreignKeys)
+                graph.AddEdge(new SEquatableEdge<Identifier>(foreignKey.ChildTable, foreignKey.ParentTable));
+
+            _deletionOrderer = new Lazy<IReadOnlyCollection<Identifier>>(() => BuildDeletionOrder(graph));
+            _insertionSorter = new Lazy<IReadOnlyCollection<Identifier>>(() => _deletionOrderer.Value.Reverse().ToList());
+        }
+
+        public IReadOnlyCollection<Identifier> DeletionOrder => _deletionOrderer.Value;
+
+        public IReadOnlyCollection<Identifier> InsertionOrder => _insertionSorter.Value;
+
+        private static IReadOnlyCollection<Identifier> BuildDeletionOrder(IVertexListGraph<Identifier, SEquatableEdge<Identifier>> graph)
+        {
+            var topologicalSorter = new TopologicalSortingAlgorithm<Identifier, SEquatableEdge<Identifier>>(graph);
+            topologicalSorter.Compute();
+
+            return topologicalSorter.SortedVertices.Distinct().ToList();
+        }
+
+        private readonly Lazy<IReadOnlyCollection<Identifier>> _deletionOrderer;
+        private readonly Lazy<IReadOnlyCollection<Identifier>> _insertionSorter;
+    }
+
+    /// <summary>
+    /// Applies topological sorting in the same manner as QuickGraph's built-in algorithm. However, this implementation enables cycles and potentially incorrect results.
+    /// </summary>
+    public sealed class TopologicalSortingAlgorithm<TVertex, TEdge> : AlgorithmBase<IVertexListGraph<TVertex, TEdge>> where TEdge : IEdge<TVertex>
+    {
+        public TopologicalSortingAlgorithm(IVertexListGraph<TVertex, TEdge> graph)
+            : this(graph, new List<TVertex>())
+        {
+        }
+
+        public TopologicalSortingAlgorithm(IVertexListGraph<TVertex, TEdge> graph, IList<TVertex> vertices)
+            : base(graph)
+        {
+            SortedVertices = vertices;
+        }
+
+        public IList<TVertex> SortedVertices { get; }
+
+        private void FinishVertex(TVertex v) => SortedVertices.Insert(0, v);
+
+        protected override void InternalCompute()
+        {
+            DepthFirstSearchAlgorithm<TVertex, TEdge> dfs = null;
+            try
+            {
+                dfs = new DepthFirstSearchAlgorithm<TVertex, TEdge>(
+                    this,
+                    VisitedGraph,
+                    new Dictionary<TVertex, GraphColor>(VisitedGraph.VertexCount)
+                );
+                dfs.FinishVertex += FinishVertex;
+
+                dfs.Compute();
+            }
+            finally
+            {
+                if (dfs != null)
+                    dfs.FinishVertex -= FinishVertex;
+            }
+        }
+    }
+}
