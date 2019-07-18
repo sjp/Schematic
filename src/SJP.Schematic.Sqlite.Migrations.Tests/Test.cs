662b1f06a15949f3c4753a4bad8e3524dd685cd4
Simon Potter <simon@sjp.co.nz>
2019-07-18 22:16:18 +1200

    Add dummy tests for build.

---
+++ src/SJP.Schematic.Sqlite.Migrations.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SJP.Schematic.Sqlite.Migrations.Tests
+{
+    [TestFixture]
+    internal static class Test
+    {
+        public static void Pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
