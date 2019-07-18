6af14eb4d905f864edc83552b977663cdca72c2d
Simon Potter <simon@sjp.co.nz>
2019-07-18 22:28:08 +1200

    Add missing attributes.

--- src/SJP.Schematic.SqlServer.Migrations.Tests/Test.cs
+++ src/SJP.Schematic.SqlServer.Migrations.Tests/Test.cs
@@ -5,6 +5,7 @@ namespace SJP.Schematic.SqlServer.Migrations.Tests
     [TestFixture]
     internal static class Test
     {
+        [Test]
         public static void Pass()
         {
             Assert.Pass();
--- src/SJP.Schematic.Sqlite.Migrations.Tests/Test.cs
+++ src/SJP.Schematic.Sqlite.Migrations.Tests/Test.cs
@@ -5,6 +5,7 @@ namespace SJP.Schematic.Sqlite.Migrations.Tests
     [TestFixture]
     internal static class Test
     {
+        [Test]
         public static void Pass()
         {
             Assert.Pass();
