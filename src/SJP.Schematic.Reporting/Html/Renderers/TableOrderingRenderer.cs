3b710a2b54460bee66b623c8bf80c34eff21f7f2
Simon Potter <simon@sjp.co.nz>
2019-07-21 21:34:52 +1200

    Now also export SQL containing an ordered set of tables for insertion or deletion.
    This is available when viewing the main page or the tables list page.

---
+++ src/SJP.Schematic.Reporting/Html/Renderers/TableOrderingRenderer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using SJP.Schematic.Core;
+using SJP.Schematic.Core.Extensions;
+using SJP.Schematic.Core.Utilities;
+
+namespace SJP.Schematic.Reporting.Html.Renderers
+{
+    internal sealed class TableOrderingRenderer : ITemplateRenderer
+    {
+        public TableOrderingRenderer(
+            IDatabaseDialect dialect,
+            IReadOnlyCollection<IRelationalDatabaseTable> tables,
+            DirectoryInfo exportDirectory)
+        {
+            if (tables == null || tables.AnyNull())
+                throw new ArgumentNullException(nameof(tables));
+
+            Tables = tables;
+
+            Dialect = dialect ?? throw new ArgumentNullException(nameof(dialect));
+            ExportDirectory = exportDirectory ?? throw new ArgumentNullException(nameof(exportDirectory));
+        }
+
+        private IDatabaseDialect Dialect { get; }
+
+        private IReadOnlyCollection<IRelationalDatabaseTable> Tables { get; }
+
+        private DirectoryInfo ExportDirectory { get; }
+
+        public async Task RenderAsync(CancellationToken cancellationToken = default)
+        {
+            if (!ExportDirectory.Exists)
+                ExportDirectory.Create();
+
+            var cycleDetector = new CycleDetector();
+            var cycles = cycleDetector.GetCyclePaths(Tables);
+            var hasCycles = cycles.Count > 0;
+
+            await ExportInsertionOrder(hasCycles).ConfigureAwait(false);
+            await ExportDeletionOrder(hasCycles).ConfigureAwait(false);
+        }
+
+        private async Task ExportInsertionOrder(bool hasCycles)
+        {
+            var orderer = new TableRelationshipOrderer();
+            var insertionOrder = orderer.GetInsertionOrder(Tables);
+            var insertionOutputPath = Path.Combine(ExportDirectory.FullName, "insertion-order.sql");
+            var insertionOrderDoc = BuildOrderDocument(insertionOrder);
+            using (var writer = File.CreateText(insertionOutputPath))
+            {
+                await writer.WriteLineAsync("-- This is the insertion order for the database.").ConfigureAwait(false);
+                await writer.WriteLineAsync("-- This may not be correct for your database or data relationships.").ConfigureAwait(false);
+                await writer.WriteLineAsync("-- Please check before relying upon this information.").ConfigureAwait(false);
+                await writer.WriteLineAsync().ConfigureAwait(false);
+
+                if (hasCycles)
+                {
+                    await writer.WriteLineAsync("-- NOTE: There are relationship cycles present in the database. This ordering is not guaranteed.").ConfigureAwait(false);
+                    await writer.WriteLineAsync().ConfigureAwait(false);
+                }
+
+                await writer.WriteAsync(insertionOrderDoc).ConfigureAwait(false);
+                await writer.FlushAsync().ConfigureAwait(false);
+            }
+        }
+
+        private async Task ExportDeletionOrder(bool hasCycles)
+        {
+            var orderer = new TableRelationshipOrderer();
+            var deletionOrder = orderer.GetDeletionOrder(Tables);
+            var deletionOutputPath = Path.Combine(ExportDirectory.FullName, "deletion-order.sql");
+            var deletionOrderDoc = BuildOrderDocument(deletionOrder);
+            using (var writer = File.CreateText(deletionOutputPath))
+            {
+                await writer.WriteLineAsync("-- This is the deletion order for the database.").ConfigureAwait(false);
+                await writer.WriteLineAsync("-- This may not be correct for your database or data relationships.").ConfigureAwait(false);
+                await writer.WriteLineAsync("-- Please check before relying upon this information.").ConfigureAwait(false);
+                await writer.WriteLineAsync().ConfigureAwait(false);
+
+                if (hasCycles)
+                {
+                    await writer.WriteLineAsync("-- NOTE: There are relationship cycles present in the database. This ordering is not guaranteed.").ConfigureAwait(false);
+                    await writer.WriteLineAsync().ConfigureAwait(false);
+                }
+
+                await writer.WriteAsync(deletionOrderDoc).ConfigureAwait(false);
+                await writer.FlushAsync().ConfigureAwait(false);
+            }
+        }
+
+        private string BuildOrderDocument(IEnumerable<Identifier> tableNames)
+        {
+            if (tableNames == null)
+                throw new ArgumentNullException(nameof(tableNames));
+
+            var builder = StringBuilderCache.Acquire();
+
+            foreach (var tableName in tableNames)
+            {
+                var reducedName = Identifier.CreateQualifiedIdentifier(tableName.Schema, tableName.LocalName);
+                var quotedName = Dialect.QuoteName(reducedName);
+                builder.AppendLine(quotedName);
+            }
+
+            return builder.GetStringAndRelease();
+        }
+    }
+}
