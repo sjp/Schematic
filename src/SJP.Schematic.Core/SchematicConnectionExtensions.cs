b02f84828da3ad7f37744e01bb652c18f628ea71
Simon Potter <simon@sjp.co.nz>
2020-03-29 19:11:33 +1300

    Docs and tests for connection extensions.

--- src/SJP.Schematic.Core/SchematicConnectionExtensions.cs
+++ src/SJP.Schematic.Core/SchematicConnectionExtensions.cs
@@ -5,8 +5,19 @@ using Nito.AsyncEx;
 
 namespace SJP.Schematic.Core
 {
+    /// <summary>
+    /// Configuration based extension methods for <see cref="ISchematicConnection"/> instances.
+    /// </summary>
     public static class SchematicConnectionExtensions
     {
+        /// <summary>
+        /// Adds logging to a database connection. Logs when queries occur using the given database connection.
+        /// </summary>
+        /// <param name="connection">A connection.</param>
+        /// <param name="loggerFactory">A logger factory.</param>
+        /// <param name="level">A logging level.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="connection"/> or <paramref name="loggerFactory"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">The { nameof(LogLevel) } provided must be a valid enum. - level</exception>
         public static void AddLogging(this ISchematicConnection connection, ILoggerFactory loggerFactory, LogLevel level)
         {
             if (connection == null)
@@ -20,6 +31,11 @@ namespace SJP.Schematic.Core
             Logging.AddLogging(connection, logger, level);
         }
 
+        /// <summary>
+        /// Removes the logging applied to a given connection.
+        /// </summary>
+        /// <param name="connection">A connection.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="connection"/> is <c>null</c>.</exception>
         public static void RemoveLogging(this ISchematicConnection connection)
         {
             if (connection == null)
@@ -31,7 +47,9 @@ namespace SJP.Schematic.Core
         /// <summary>
         /// Sets the maximum number of concurrent queries to perform with the given connection.
         /// </summary>
+        /// <param name="connection">A connection.</param>
         /// <param name="maxQueries">The maximum number of queries.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="connection"/> is <c>null</c>.</exception>
         public static void SetMaxConcurrentQueries(this ISchematicConnection connection, uint maxQueries)
         {
             if (connection == null)
