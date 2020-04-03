5b7832f34ad0b584ffe73a9d2aaf8e4ee4401a77
Simon Potter <simon@sjp.co.nz>
2020-04-03 22:12:29 +1300

    Minor docs added.

--- src/SJP.Schematic.Core/IdentifierDefaultsBuilder.cs
+++ src/SJP.Schematic.Core/IdentifierDefaultsBuilder.cs
@@ -2,8 +2,14 @@
 
 namespace SJP.Schematic.Core
 {
+    /// <summary>
+    /// A builder for constructing database identifier defaults.
+    /// </summary>
     public sealed class IdentifierDefaultsBuilder
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdentifierDefaultsBuilder"/> class.
+        /// </summary>
         public IdentifierDefaultsBuilder()
         {
             _defaults = new IdentifierDefaults(null, null, null);
@@ -14,6 +20,11 @@ namespace SJP.Schematic.Core
             _defaults = identifierDefaults ?? throw new ArgumentNullException(nameof(identifierDefaults));
         }
 
+        /// <summary>
+        /// Constructs a new builder that has a provided server name set in addition to any other existing defaults.
+        /// </summary>
+        /// <param name="server">A server name.</param>
+        /// <returns>A new builder with the given server name set.</returns>
         public IdentifierDefaultsBuilder WithServer(string server)
         {
             return new IdentifierDefaultsBuilder(
@@ -21,6 +32,11 @@ namespace SJP.Schematic.Core
             );
         }
 
+        /// <summary>
+        /// Constructs a new builder that has a provided database name set in addition to any other existing defaults.
+        /// </summary>
+        /// <param name="database">A database name.</param>
+        /// <returns>A new builder with the given database name set.</returns>
         public IdentifierDefaultsBuilder WithDatabase(string database)
         {
             return new IdentifierDefaultsBuilder(
@@ -28,6 +44,11 @@ namespace SJP.Schematic.Core
             );
         }
 
+        /// <summary>
+        /// Constructs a new builder that has a provided schema name set in addition to any other existing defaults.
+        /// </summary>
+        /// <param name="schema">A schema name.</param>
+        /// <returns>A new builder with the given schema name set.</returns>
         public IdentifierDefaultsBuilder WithSchema(string schema)
         {
             return new IdentifierDefaultsBuilder(
@@ -35,6 +56,10 @@ namespace SJP.Schematic.Core
             );
         }
 
+        /// <summary>
+        /// Constructs a resulting set of identifier defaults.
+        /// </summary>
+        /// <returns>Database identifier defaults.</returns>
         public IIdentifierDefaults Build() => _defaults;
 
         private readonly IdentifierDefaults _defaults;
