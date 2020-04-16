268b9714f1e8662b5768e8813391cbdb914ecfb9
Simon Potter <simon@sjp.co.nz>
2020-04-16 21:21:15 +1200

    Add test for sequence generation.
    Normally not required, but sqlite does not provide support for sequences so needs to be done manually instead of via integration test.

--- src/SJP.Schematic.DataAccess.EntityFrameworkCore.Tests/EFCoreDbContextBuilderTests.cs
+++ src/SJP.Schematic.DataAccess.EntityFrameworkCore.Tests/EFCoreDbContextBuilderTests.cs
@@ -1,4 +1,5 @@
 using System;
+using LanguageExt;
 using NUnit.Framework;
 using SJP.Schematic.Core;
 
@@ -54,5 +55,47 @@ namespace SJP.Schematic.DataAccess.EntityFrameworkCore.Tests
 
             Assert.That(() => dbContextBuilder.Generate(tables, views, null), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public static void Generate_GivenValidSequence_ReturnsExpectedConfiguration()
+        {
+            var nameTranslator = new VerbatimNameTranslator();
+            var dbContextBuilder = new EFCoreDbContextBuilder(nameTranslator, "test");
+            var tables = Array.Empty<IRelationalDatabaseTable>();
+            var views = Array.Empty<IDatabaseView>();
+
+            var sequence = new DatabaseSequence(
+                "test_sequence",
+                3,
+                20,
+                Option<decimal>.Some(0),
+                Option<decimal>.Some(100),
+                true,
+                2
+            );
+            var sequences = new[] { sequence };
+
+            var result = dbContextBuilder.Generate(tables, views, sequences);
+
+            Assert.That(result, Is.EqualTo(ExpectedSequenceTestResult));
+        }
+
+        private const string ExpectedSequenceTestResult = @"using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace test
+{
+    public class AppContext : DbContext
+    {
+        /// <summary>
+        /// Configure the model that was discovered by convention from the defined entity types.
+        /// </summary>
+        /// <param name=""modelBuilder"">The builder being used to construct the model for this context.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasSequence<decimal>(""test_sequence"").StartsAt(3M).IncrementsBy(20M);
+        }
+    }
+}";
     }
 }
