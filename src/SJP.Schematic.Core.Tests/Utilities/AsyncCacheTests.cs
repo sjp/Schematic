d98d8b43e0396069a2ed7607be0309680f4dfbdb
Simon Potter <simon@sjp.co.nz>
2020-05-05 21:07:08 +1200

    Tests and docs for AsyncCache.

---
+++ src/SJP.Schematic.Core.Tests/Utilities/AsyncCacheTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SJP.Schematic.Core.Utilities;
+
+namespace SJP.Schematic.Core.Tests.Utilities
+{
+    [TestFixture]
+    internal static class AsyncCacheTests
+    {
+        [Test]
+        public static void Ctor_GivenNullFactory_ThrowsArgumentNullException()
+        {
+            Assert.That(() => new AsyncCache<object, object, object>(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public static void GetByKeyAsync_GivenNullKey_ThrowsArgumentNullException()
+        {
+            var cache = new AsyncCache<object, object, object>((object key, object cache, CancellationToken cancellationToken) => Task.FromResult(new object()));
+
+            Assert.That(() => cache.GetByKeyAsync(null, new object()), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public static void GetByKeyAsync_GivenNullCache_ThrowsArgumentNullException()
+        {
+            var cache = new AsyncCache<object, object, object>((object key, object cache, CancellationToken cancellationToken) => Task.FromResult(new object()));
+
+            Assert.That(() => cache.GetByKeyAsync(new object(), null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public static async Task GetByKeyAsync_WhenCalledTwiceWithSameKey_InvokesFactoryOnlyOnce()
+        {
+            var counter = 0;
+            var cache = new AsyncCache<string, string, string>((string key, string cache, CancellationToken cancellationToken) =>
+            {
+                counter++;
+                return Task.FromResult(string.Empty);
+            });
+
+            await cache.GetByKeyAsync("a", "cache_ignore").ConfigureAwait(false);
+            await cache.GetByKeyAsync("a", "cache_ignore").ConfigureAwait(false);
+
+            Assert.That(counter, Is.EqualTo(1));
+        }
+    }
+}
