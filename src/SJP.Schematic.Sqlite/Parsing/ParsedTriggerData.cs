f9668c9b2942e1172aa2e409f63428962c9ea3c3
Simon Potter <simon@sjp.co.nz>
2020-05-18 20:37:17 +1200

    Documentation.

--- src/SJP.Schematic.Sqlite/Parsing/ParsedTriggerData.cs
+++ src/SJP.Schematic.Sqlite/Parsing/ParsedTriggerData.cs
@@ -4,8 +4,17 @@ using SJP.Schematic.Core;
 
 namespace SJP.Schematic.Sqlite.Parsing
 {
+    /// <summary>
+    /// A data container that holds parsed table information from a SQLite <c>CREATE TRIGGER</c> statement.
+    /// </summary>
     public sealed class ParsedTriggerData
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParsedTriggerData"/> class.
+        /// </summary>
+        /// <param name="queryTiming">A query timing.</param>
+        /// <param name="events">Trigger events.</param>
+        /// <exception cref="ArgumentException"><paramref name="queryTiming"/> or <paramref name="events"/> are invalid enum values.</exception>
         public ParsedTriggerData(TriggerQueryTiming queryTiming, TriggerEvent events)
         {
             if (!queryTiming.IsValid())
@@ -17,8 +26,17 @@ namespace SJP.Schematic.Sqlite.Parsing
             Event = events;
         }
 
+        /// <summary>
+        /// The parsed trigger query timing from the <c>CREATE TRIGGER</c> statement.
+        /// </summary>
+        /// <value>A trigger query timing.
+        /// </value>
         public TriggerQueryTiming Timing { get; }
 
+        /// <summary>
+        /// The parsed trigger event from the <c>CREATE TRIGGER</c> statement.
+        /// </summary>
+        /// <value>A trigger event.</value>
         public TriggerEvent Event { get; }
     }
 }
