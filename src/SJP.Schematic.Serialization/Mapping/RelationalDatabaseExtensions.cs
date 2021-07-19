ce99a2a3efd6b925dcaf3e80dd1fde04c5af9b1f
Simon Potter <simon@sjp.co.nz>
2021-07-19 21:38:54 +1200

    Minor simplification and memory optimisation.
    Generate dtos directly when materializing the sequence.

--- src/SJP.Schematic.Serialization/Mapping/RelationalDatabaseExtensions.cs
+++ src/SJP.Schematic.Serialization/Mapping/RelationalDatabaseExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,26 +21,35 @@ namespace SJP.Schematic.Serialization.Mapping
 
         private static async Task<Dto.RelationalDatabase> ToDtoAsyncCore(IMapper mapper, IRelationalDatabase database, CancellationToken cancellationToken)
         {
-            var tablesTask = database.GetAllTables(cancellationToken).ToListAsync(cancellationToken).AsTask();
-            var viewsTask = database.GetAllViews(cancellationToken).ToListAsync(cancellationToken).AsTask();
-            var sequencesTask = database.GetAllSequences(cancellationToken).ToListAsync(cancellationToken).AsTask();
-            var synonymsTask = database.GetAllSynonyms(cancellationToken).ToListAsync(cancellationToken).AsTask();
-            var routinesTask = database.GetAllRoutines(cancellationToken).ToListAsync(cancellationToken).AsTask();
+            var tablesTask = database.GetAllTables(cancellationToken)
+                .Select(t => mapper.Map<IRelationalDatabaseTable, Dto.RelationalDatabaseTable>(t))
+                .ToListAsync(cancellationToken)
+                .AsTask();
+            var viewsTask = database.GetAllViews(cancellationToken)
+                .Select(v => mapper.Map<IDatabaseView, Dto.DatabaseView>(v))
+                .ToListAsync(cancellationToken)
+                .AsTask();
+            var sequencesTask = database.GetAllSequences(cancellationToken)
+                .Select(s => mapper.Map<IDatabaseSequence, Dto.DatabaseSequence>(s))
+                .ToListAsync(cancellationToken)
+                .AsTask();
+            var synonymsTask = database.GetAllSynonyms(cancellationToken)
+                .Select(s => mapper.Map<IDatabaseSynonym, Dto.DatabaseSynonym>(s))
+                .ToListAsync(cancellationToken)
+                .AsTask();
+            var routinesTask = database.GetAllRoutines(cancellationToken)
+                .Select(r => mapper.Map<IDatabaseRoutine, Dto.DatabaseRoutine>(r))
+                .ToListAsync(cancellationToken)
+                .AsTask();
 
             await Task.WhenAll(tablesTask, viewsTask, sequencesTask, synonymsTask, routinesTask).ConfigureAwait(false);
 
-            var tables = await tablesTask.ConfigureAwait(false);
-            var views = await viewsTask.ConfigureAwait(false);
-            var sequences = await sequencesTask.ConfigureAwait(false);
-            var synonyms = await synonymsTask.ConfigureAwait(false);
-            var routines = await routinesTask.ConfigureAwait(false);
-
             var dtoIdentifierDefaults = mapper.Map<IIdentifierDefaults, Dto.IdentifierDefaults>(database.IdentifierDefaults);
-            var dtoTables = mapper.Map<IEnumerable<IRelationalDatabaseTable>, IEnumerable<Dto.RelationalDatabaseTable>>(tables);
-            var dtoViews = mapper.Map<IEnumerable<IDatabaseView>, IEnumerable<Dto.DatabaseView>>(views);
-            var dtoSequences = mapper.Map<IEnumerable<IDatabaseSequence>, IEnumerable<Dto.DatabaseSequence>>(sequences);
-            var dtoSynonyms = mapper.Map<IEnumerable<IDatabaseSynonym>, IEnumerable<Dto.DatabaseSynonym>>(synonyms);
-            var dtoRoutines = mapper.Map<IEnumerable<IDatabaseRoutine>, IEnumerable<Dto.DatabaseRoutine>>(routines);
+            var dtoTables = await tablesTask.ConfigureAwait(false);
+            var dtoViews = await viewsTask.ConfigureAwait(false);
+            var dtoSequences = await sequencesTask.ConfigureAwait(false);
+            var dtoSynonyms = await synonymsTask.ConfigureAwait(false);
+            var dtoRoutines = await routinesTask.ConfigureAwait(false);
 
             return new Dto.RelationalDatabase
             {
