5812809534e4e59745c29362f109b3aec1c7bc13
Simon Potter <simon@sjp.co.nz>
2022-03-16 19:04:30 +1300

    Add oracle config for local dev.

--- development/update-test-config-files.sh
+++ development/update-test-config-files.sh
@@ -3,5 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cp "${DIR}/mysql-test.config.json" "${DIR}/../src/SJP.Schematic.MySql.Tests/mysql-test.config.json"
+cp "${DIR}/oracle-test.config.json" "${DIR}/../src/SJP.Schematic.Oracle.Tests/oracle-test.config.json"
 cp "${DIR}/postgresql-test.config.json" "${DIR}/../src/SJP.Schematic.PostgreSql.Tests/postgresql-test.config.json"
 cp "${DIR}/sqlserver-test.config.json" "${DIR}/../src/SJP.Schematic.SqlServer.Tests/sqlserver-test.config.json"
