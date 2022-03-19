9bd48d21f18633628cc66861acec4e34a13e4381
Simon Potter <simon@sjp.co.nz>
2022-03-20 08:52:02 +1300

    Skip synonym tests while permissions are unavailable.

--- src/SJP.Schematic.Oracle.Tests/Integration/OracleDatabaseSynonymProviderTests.cs
+++ src/SJP.Schematic.Oracle.Tests/Integration/OracleDatabaseSynonymProviderTests.cs
@@ -9,6 +9,7 @@ using SJP.Schematic.Tests.Utilities;
 
 namespace SJP.Schematic.Oracle.Tests.Integration;
 
+[Ignore("Disabling until synonym permissions available in CI tests")]
 internal sealed class OracleDatabaseSynonymProviderTests : OracleTest
 {
     private IDatabaseSynonymProvider SynonymProvider => new OracleDatabaseSynonymProvider(DbConnection, IdentifierDefaults, IdentifierResolver);
