b03d021e3c7df620d4c9231aabd923d1de388b9c
Simon Potter <simon@sjp.co.nz>
2023-06-15 20:27:34 +1200

    Remove unused usings

--- src/SJP.Schematic.Core.Tests/CachingConnectionFactoryTests.cs
+++ src/SJP.Schematic.Core.Tests/CachingConnectionFactoryTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using System.Data.Common;
+﻿using System.Data.Common;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
--- src/SJP.Schematic.Oracle/OracleDatabaseMaterializedViewProvider.cs
+++ src/SJP.Schematic.Oracle/OracleDatabaseMaterializedViewProvider.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using LanguageExt;
 using SJP.Schematic.Core;
 using SJP.Schematic.Core.Extensions;
