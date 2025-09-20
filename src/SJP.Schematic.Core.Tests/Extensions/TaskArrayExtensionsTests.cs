c5c8a906f64f37df0d82cae32d7a39e6acf14710
Simon Potter <simon@sjp.co.nz>
2025-09-20 16:53:40 +1200

    Add new extension method to simplify task unwrapping from a collection

---
+++ src/SJP.Schematic.Core.Tests/Extensions/TaskArrayExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SJP.Schematic.Core.Extensions;
+
+namespace SJP.Schematic.Core.Tests.Extensions;
+
+[TestFixture]
+public static class TaskArrayExtensionsTests
+{
+    [Test]
+    public static void WhenAll_WhenGivenNullTasks_ThrowsArgNullException()
+    {
+        Assert.That(() => TaskArrayExtensions.WhenAll<object>(null), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public static async Task WhenAll_WhenGivenValid_ReturnsExpectedResults()
+    {
+        var tasks = new[]
+        {
+            Task.FromResult(2),
+            Task.FromResult(4),
+            Task.FromResult(6),
+        };
+
+        var results = await tasks.WhenAll().ConfigureAwait(false);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(results, Has.Exactly(3).Items);
+            Assert.That(results[0], Is.EqualTo(2));
+            Assert.That(results[1], Is.EqualTo(4));
+            Assert.That(results[2], Is.EqualTo(6));
+        }
+    }
+}
---
+++ src/SJP.Schematic.Core/Extensions/TaskArrayExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SJP.Schematic.Core.Extensions;
+
+/// <summary>
+/// Helper extensions for working with collection of tasks.
+/// </summary>
+public static class TaskArrayExtensions
+{
+    /// <summary>
+    /// Evaluates and unwraps all of the tasks so they can be read immediately.
+    /// </summary>
+    /// <typeparam name="T">The type of the result of all of the task operations.</typeparam>
+    /// <param name="tasks">A collection of tasks.</param>
+    /// <returns>A set of results that are returned from all of the tasks. The ordering of the results matches the ordering of the tasks themselves.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="tasks"/> is <see langword="null"/>.</exception>
+    public static async Task<T[]> WhenAll<T>(this Task<T>[] tasks)
+    {
+        ArgumentNullException.ThrowIfNull(tasks);
+
+        await Task.WhenAll(tasks).ConfigureAwait(false);
+
+        return tasks
+            .Select(t => t.Result)
+            .ToArray();
+    }
+}
