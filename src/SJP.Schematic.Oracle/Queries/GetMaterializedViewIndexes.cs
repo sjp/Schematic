fc3f18b590c60ec7e4f32ed7988d6826d401d1c8
Simon Potter <simon@sjp.co.nz>
2026-09-15 09:40:56 +0000

    Stop joining ALL_OBJECTS in Oracle table and materialized view index queries, which filtered nothing out

--- src/SJP.Schematic.Oracle/Queries/GetMaterializedViewIndexes.cs
+++ src/SJP.Schematic.Oracle/Queries/GetMaterializedViewIndexes.cs
@@ -27,11 +27,9 @@ select
     aic.COLUMN_POSITION as "{nameof(GetTableIndexes.Result.ColumnPosition)}",
     aic.DESCEND as "{nameof(GetTableIndexes.Result.IsDescending)}"
 from SYS.ALL_INDEXES ai
-inner join SYS.ALL_OBJECTS ao on ai.OWNER = ao.OWNER and ai.INDEX_NAME = ao.OBJECT_NAME
 inner join SYS.ALL_IND_COLUMNS aic
     on ai.OWNER = aic.INDEX_OWNER and ai.INDEX_NAME = aic.INDEX_NAME
 where ai.TABLE_OWNER = :{nameof(Query.SchemaName)} and ai.TABLE_NAME = :{nameof(Query.ViewName)}
     and aic.TABLE_OWNER = :{nameof(Query.SchemaName)} and aic.TABLE_NAME = :{nameof(Query.ViewName)}
-    and ao.OBJECT_TYPE = 'INDEX'
 """;
 }
--- src/SJP.Schematic.Oracle/Queries/GetTableIndexes.cs
+++ src/SJP.Schematic.Oracle/Queries/GetTableIndexes.cs
@@ -51,11 +51,9 @@ select
     aic.COLUMN_POSITION as "{nameof(Result.ColumnPosition)}",
     aic.DESCEND as "{nameof(Result.IsDescending)}"
 from SYS.ALL_INDEXES ai
-inner join SYS.ALL_OBJECTS ao on ai.OWNER = ao.OWNER and ai.INDEX_NAME = ao.OBJECT_NAME
 inner join SYS.ALL_IND_COLUMNS aic
     on ai.OWNER = aic.INDEX_OWNER and ai.INDEX_NAME = aic.INDEX_NAME
 where ai.TABLE_OWNER = :{nameof(Query.SchemaName)} and ai.TABLE_NAME = :{nameof(Query.TableName)}
     and aic.TABLE_OWNER = :{nameof(Query.SchemaName)} and aic.TABLE_NAME = :{nameof(Query.TableName)}
-    and ao.OBJECT_TYPE = 'INDEX'
 """;
 }
